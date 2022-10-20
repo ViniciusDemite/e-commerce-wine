614fbce5f388f7fcbcdfcbb821c745c22ca01b78
Vinicius D. Gubolin <vinicius.demite.gubolin@gmail.com>
2022-10-20 15:58:02 -0300

    fix: corrigido nome do campo full_name

--- api/app/database/factories/UserFactory.php
+++ api/app/database/factories/UserFactory.php
@@ -15,9 +15,9 @@ class UserFactory extends Factory
 	public function definition(): array
 	{
 		return [
-            'id' => $this->faker->unique()->numberBetween(0, 200),
+			'id' => $this->faker->unique()->numberBetween(0, 200),
 			'username' => strtolower(implode("-", explode(" ", $this->faker->name))),
-            'fullname' => $this->faker->name,
+			'full_name' => $this->faker->name,
 			'email' => $this->faker->unique()->safeEmail,
 			'email_verified_at' => \Leaf\Date::now(),
 			'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
