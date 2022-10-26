c4cb008b333807270d75f75414c34fe903fcfebf
Vinicius D. Gubolin <vinicius.demite.gubolin@gmail.com>
2022-10-26 11:58:56 -0300

    feature: iniciando implementação para carrinho

--- web/pages/produtos/[slug]/index.tsx
+++ web/pages/produtos/[slug]/index.tsx
@@ -6,8 +6,33 @@ import styles from "../../../styles/Product.module.css";
 import placeholder from "../../../public/placeholder.png";
 import Image from "next/image";
 import Link from "next/link";
+import { NextRouter, useRouter } from "next/router";
+import { ChangeEvent, useState } from "react";
 
 export default function Product({ product }: ProductProps) {
+	const router = useRouter();
+	const [quantity, setQuantity] = useState(1);
+
+	function changeQuantityValue(e: ChangeEvent<HTMLInputElement>) {
+		setQuantity(parseInt(e.target.value));
+	}
+
+	async function addItemToCart(
+		product_id: number,
+		quantity: number,
+		router: NextRouter
+	) {
+		const response = await axios.post("/cart", {
+			params: {
+				quantity,
+				product_id,
+			},
+		});
+		const { data: cart } = await response.data;
+
+		console.log(cart);
+	}
+
 	return (
 		<>
 			<h1 className={styles.title}>{product.name}</h1>
@@ -25,9 +50,31 @@ export default function Product({ product }: ProductProps) {
 				<strong>Peso:</strong> {product.weight}
 			</p>
 
-			<Link href="/cart">
-				<a className={styles.cart_button}>Comprar</a>
-			</Link>
+			<div className={styles.input_container}>
+				<label htmlFor="quantity" className={styles.label}>
+					Quantidade:
+				</label>
+				<input
+					type="number"
+					name="quantity"
+					id="quantity"
+					value={quantity}
+					onChange={changeQuantityValue}
+					className={styles.input}
+				/>
+			</div>
+
+			<button
+				type="button"
+				className={styles.cart_button}
+				onClick={async () => {
+					await addItemToCart(product.id, quantity, router);
+				}}
+			>
+				Comprar
+			</button>
+
+			{/*  */}
 		</>
 	);
 }
